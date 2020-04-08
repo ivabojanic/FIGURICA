3c829b0564a6715a7a6b4cf2546cd76e4da40e56
ivabojanic <62704225+ivabojanic@users.noreply.github.com>
2020-04-08 12:17:55 +0200

    Create main.cpp
    Sad znam šta su apstraktne metode :)

---
+++ main.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+
+using namespace std;
+
+#include <cmath>
+
+class Figura
+{
+    protected:
+        double a;
+        double b;
+    public:
+        virtual double Obim() = 0;
+        virtual double Povrsina() = 0;
+};
+class Pravougaonik : Figura
+{
+private:
+    double a;
+    double b;
+public:
+    Pravougaonik(){a = 10.00; b = 15.00;}
+    Pravougaonik(double aa, double bb){a = aa; b = bb;}
+    Pravougaonik(const Pravougaonik &P){ a = P.a; b = P.b;}
+
+    double getA()const {return a;}
+    double getB()const {return b;}
+
+    void setA(double A){a = A;}
+    void setB(double B){b = B;}
+
+    double Povrsina()
+    {
+        double P = a*b;
+        return P;
+    }
+    double Obim()
+    {
+        double O = 2*(a+b);
+        return O;
+    }
+};
+class Elipsa : Figura
+{
+private:
+    double a;
+    double b;
+public:
+    Elipsa(){a = 2.00; b = 3.00;}
+    Elipsa(double aa, double bb){a = aa; b = bb;}
+    Elipsa(const Elipsa &E){ a = E.a; b = E.b;}
+
+    double getA()const {return a;}
+    double getB()const {return b;}
+
+    void setA(double A){a = A;}
+    void setB(double B){b = B;}
+
+    double Povrsina()
+    {
+        double P = a*b*3.14;
+        return P;
+    }
+    double Obim()
+    {
+        double O = 3.14*(3*(a + b)-sqrt((3*a + b)*(a + 3*b)));
+        return O;
+    }
+};
+class Kvadrat : Pravougaonik
+{
+private:
+    double a;
+public:
+    Kvadrat(){a = 3.00;}
+    Kvadrat(double aa){a = aa;}
+    Kvadrat(const Kvadrat &K){ a = K.a;}
+
+    double getA()const {return a;}
+
+    void setA(double A){a = A;}
+
+    double Povrsina()
+    {
+        double P = a*a;
+        return P;
+    }
+    double Obim()
+    {
+        double O = 4*a  ;
+        return O;
+    }
+};
+class Krug : Elipsa
+{
+private:
+    double a;
+public:
+    Krug(){a = 2.00;}
+    Krug(double aa){a = aa;}
+    Krug(const Krug &K){ a = K.a;}
+
+    double getA()const {return a;}
+
+    void setA(double A){a = A;}
+
+    double Povrsina()
+    {
+        double P = a*a*3.14;
+        return P;
+    }
+    double Obim()
+    {
+        double O = 3.14*(3*(2*a)-sqrt((4*a)*(4*a)));
+        return O;
+    }
+};
+class Oblik
+{
+private:
+    Kvadrat A;
+    Krug B;
+public:
+    Oblik() : A(), B(){}
+    Oblik(double x) : A(x), B(x){}
+    Oblik(const Oblik &O) : A(O.A), B(O.B){}
+
+    double UkupniObim()
+    {
+        return A.Obim()+ B.Obim();
+    }
+    double UkupnaPovrsina()
+    {
+        return A.Povrsina()+ B.Povrsina();
+    }
+};
+int main()
+{
+    Pravougaonik P;
+    Pravougaonik P1(10,12);
+    Pravougaonik P2(P);
+    cout<<P.Obim()<<","<<P.Povrsina()<<endl;
+    cout<<P1.Obim()<<","<<P1.Povrsina()<<endl;
+    cout<<P2.Obim()<<","<<P2.Povrsina()<<endl;
+
+    Elipsa E;
+    Elipsa E1(2,5);
+    Elipsa E2(E);
+    cout<<E.Obim()<<","<<E.Povrsina()<<endl;
+    cout<<E1.Obim()<<","<<E1.Povrsina()<<endl;
+    cout<<E2.Obim()<<","<<E2.Povrsina()<<endl;
+
+    Oblik O;
+    Oblik O1(2);
+    Oblik O2(O);
+    cout<<O.UkupniObim()<<","<<O.UkupnaPovrsina()<<endl;
+    cout<<O1.UkupniObim()<<","<<O1.UkupnaPovrsina()<<endl;
+    cout<<O2.UkupniObim()<<","<<O2.UkupnaPovrsina()<<endl;
+    return 0;
+}
